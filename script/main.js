f75edafd38ad7a5d16841d937c9277805e823ee5
Glassto <102603092+Glassto@users.noreply.github.com>
2023-11-11 16:36:18 +0200

    Update main.js

--- script/main.js
+++ script/main.js
@@ -3,7 +3,7 @@ var nav = document.getElementsByClassName("nav-background");
 console.log(getLocation[0].offsetTop)
 
 window.onscroll = function() {
-  if(window.scrollY > getLocation[0].offsetTop) {
+  if(window.scrollY > nav[0].offsetTop) {
     nav[0].classList.add("sticky");
   } else {
     nav[0].classList.remove("sticky");
