1ba5adb687405af1248600b88cbd2ba20d10309f
Glassto <102603092+Glassto@users.noreply.github.com>
2023-11-11 17:21:15 +0200

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,11 +1,14 @@
 var getLocation = document.getElementsByClassName("location-guide");
 var nav = document.getElementsByClassName("nav-background");
-console.log(getLocation[0].offsetTop)
+var presentation = document.getElementsByClassName("presentation");
+console.log(presentation[0])
 
 window.onscroll = function() {
   if(window.scrollY > nav[0].offsetTop) {
     nav[0].classList.add("sticky");
+    presentation[0].classList.add("sticky");
   } else {
     nav[0].classList.remove("sticky");
+    presentation[0].classList.remove("sticky");
   }
 }
