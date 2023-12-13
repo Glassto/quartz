d36f7ce9d1327adc6facfba5906690e4181ab887
Glassto <102603092+Glassto@users.noreply.github.com>
2023-12-13 19:45:00 +0200

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,64 +1,67 @@
-const nav = document.querySelector('body > .nav-background');
-const presentation = document.querySelector('body > .presentation');
-const showFoodList = document.querySelectorAll('.foods');
-const rotateArrow = document.querySelectorAll('.arrow');
-const focusOnIcon = document.querySelectorAll('.category-name-ico');
-const focusOnText = document.querySelectorAll('.category-name-text');
-const focusOnBox = document.querySelectorAll('.category-name');
-const reviewBoxes = document.querySelectorAll('.review-box');
-const nextButton = document.getElementById('next-btn');
-const prevButton = document.getElementById('prev-btn');
-const reviewOptions = document.getElementsByClassName('dropdown-content');
-const hideWrkButton = document.getElementsByClassName('wrk-progress');
-const sidemenu = document.getElementById('js-side-menu');
-
 function addClass (element1, className) {
   element1.classList.add(className);
 }
 function removeClass (element1, className) {
   element1.classList.remove(className);
 }
-
+const sidemenu = document.getElementById('js-side-menu');
 window.onscroll = () => {
+  const nav = document.querySelector('body > .nav-background');
+  const presentation = document.querySelector('body > .presentation');
+
    window.scrollY > nav.offsetTop ? addClass (nav, "sticky") : removeClass (nav,  "sticky");
    window.scrollY > nav.offsetTop ? addClass (presentation, "sticky") : removeClass (presentation,  "sticky");
    window.scrollY > nav.offsetTop ? addClass (sidemenu, "sticky") : removeClass (sidemenu, "sticky")
 }
-
 function openSideMenu() {
   sidemenu.classList.toggle("open")
 }
+onclickScroll = (object) => {
+  object === 'menu' ? element = document.getElementById('js-menu-section') : element = document.getElementById('js-review-section');
+
+  let navbarOffset = 50;
+  let elementPosition = element.getBoundingClientRect().top;
+  let offsetPosition = elementPosition + window.scrollY - navbarOffset;
+
+  window.scrollTo({
+    top: offsetPosition,
+  });
+}
+
 
 function checkWhichCategory(menuList) {
   rotate(menuList);
   focus(menuList);
 }
 function rotate(nodeList) {
+  const rotateArrow = document.querySelectorAll('.arrow');
   rotateArrow[nodeList].classList.toggle("rotate");
 }
 function focus(nodeList) {
+  const focusOnIcon = document.querySelectorAll('.category-name-ico');
+  const focusOnText = document.querySelectorAll('.category-name-text');
+  const focusOnBox = document.querySelectorAll('.category-name');
+  const showFoodList = document.querySelectorAll('.foods');
+
   focusOnIcon[nodeList].classList.toggle("focus-icon");
   focusOnText[nodeList].classList.toggle("focus-text");
   focusOnBox[nodeList].classList.toggle("focus-box");
   showFoodList[nodeList].classList.toggle("hidden");
 }
-function closeWrk(nodeList) {
-  addClass(hideWrkButton[nodeList], "hide-wrk")
-}
 
 
-let visibleBox = 0;
+const reviewBoxes = document.querySelectorAll('.review-box');
 function showBoxes(index) {
   reviewBoxes.forEach(item => {
     item.style.display = 'none';
   })
   reviewBoxes[index].style.display = 'flex';
 }
+let visibleBox = 0;
 function nextBox() {
   if (visibleBox >= 0 && visibleBox < reviewBoxes.length - 1) {
     visibleBox += 1;
     showBoxes(visibleBox);
-    prevButton.style.display = "block"
   } else {
     visibleBox = 0;
     showBoxes(visibleBox);
@@ -68,7 +71,6 @@ function prevBox() {
   if (visibleBox > 0) {
     visibleBox -= 1;
     showBoxes(visibleBox);
-    nextButton.style.display = "flex"
   } else {
     visibleBox = reviewBoxes.length - 1;
     showBoxes(visibleBox)
@@ -77,10 +79,9 @@ function prevBox() {
 showBoxes(visibleBox);
 
 function openDropdown() {
+  const reviewOptions = document.getElementsByClassName('dropdown-content');
   let i;
   for (i=0; i<=reviewOptions.length; i++) {
-    reviewOptions[i].classList.toggle('show')
-    console.log(i)
+    reviewOptions[i].classList.toggle('show');
   }
-  
 }
